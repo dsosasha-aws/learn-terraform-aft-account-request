6bd6bd41624ff7323d4508ed74710546d302dfbd
dsosasha-aws <dsosasha@amazon.com>
2022-05-09 15:10:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "dsosasha+lzchild@amazon.com"
-    AccountName               = "CTChild"
-    ManagedOrganizationalUnit = "Custom"
-    SSOUserEmail              = "dsosasha+lzchild@amazon.com"
+    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "<SSO EMAIL>"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
